155edb4583348cbfeb35c7f12c9d96283713fb68
Andrey <andreystar2403@gmail.com>
2021-11-19 13:40:24 +0300

    Add testcontainers and Spring Profile

--- backend/src/test/java/sticker/controller/UserControllerTest.java
+++ backend/src/test/java/sticker/controller/UserControllerTest.java
@@ -8,22 +8,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.transaction.Transactional;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@Sql("/scripts/add_test_user.sql")
-@Transactional
+//@Sql("/scripts/add_test_user.sql")
+//@Transactional
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(outputDir = "snippets")
+@Testcontainers
+@ActiveProfiles("testcontainers")
 class UserControllerTest {
 
     @Autowired
@@ -32,9 +35,16 @@ class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Container
+    public static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer("postgres:latest")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("pass");
+
     @Test
     @WithUserDetails
     public void testCurrentUser() throws Exception {
+        assertTrue(postgresqlContainer.isRunning());
         mockMvc.perform(get("/api/users/0"))
                .andExpect(status().isOk())
                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -42,9 +52,9 @@ class UserControllerTest {
                .andDo(document("Current user", responseFields(
                        fieldWithPath("id").description("The internal id of the user"),
                        fieldWithPath("username").description("The username chosen on registration"),
-                       fieldWithPath("roles").description("The roles this user belongs to"),
+                       subsectionWithPath("roles.[]").description("The roles this user belongs to"),
                        fieldWithPath("enabled").description("Notes whether the account is enabled"),
-                       fieldWithPath("authorities").description("Same as roles"),
+                       subsectionWithPath("authorities.[]").description("Same as roles"),
                        fieldWithPath("accountNonExpired").description("Notes whether the account is expired"),
                        fieldWithPath("accountNonLocked").description("Notes whether the account is locked"),
                        fieldWithPath("credentialsNonExpired").description("Notes whether the credentials expired")
@@ -58,3 +68,5 @@ class UserControllerTest {
                .andExpect(status().isUnauthorized());
     }
 }
+
+
